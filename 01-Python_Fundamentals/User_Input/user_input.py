12b2a71dac81fe5ebfe5005e86f92ee348067078
Shourya1009 <jshourya007@gmail.com>
2025-07-13 23:51:15 +0530

    Update user_input.py

--- 01-Python_Fundamentals/User_Input/user_input.py
+++ 01-Python_Fundamentals/User_Input/user_input.py
@@ -6,3 +6,11 @@ a=input("Enter a Number : ")  # input always taken as string
 print(a)
 print(type(a))
 
+num = int(input("Enter a number : "))
+print(f"The number you have entered is {num}")
+print(type(num))
+
+name = input("Enter your name : ")
+age = int(input("Enter your age : "))
+print("Your name is {} and you age is {}".format(name,age))
+print(f"Your age is {age} and your name is {name}")
