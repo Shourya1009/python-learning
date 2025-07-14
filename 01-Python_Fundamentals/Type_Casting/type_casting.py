fc9c14340800e854579ec3460c0df8684bfa88e5
Shourya1009 <jshourya007@gmail.com>
2025-07-14 21:11:30 +0530

    Update type_casting.py

--- 01-Python_Fundamentals/Type_Casting/type_casting.py
+++ 01-Python_Fundamentals/Type_Casting/type_casting.py
@@ -1,3 +1,24 @@
+/*
+Type Casting is the method to convert the Python variable datatype into a certain data type in order to perform the required operation by users.
+
+There can be two types of Type Casting in Python:
+
+1] Python Implicit Type Conversion
+2] Python Explicit Type Conversion
+
+1] Implicit Type Conversion in Python
+-> In this, method, Python converts the datatype into another datatype automatically. Users don't have to involve in this process. 
+
+2] Explicit Type Conversion in Python
+-> In this method, Python needs user involvement to convert the variable data type into the required data type. 
+
+
+Mainly type casting can be done with these data type functions:-
+Int(): Python Int() function take float or string as an argument and returns int type object.
+float(): Python float() function take int or string as an argument and return float type object.
+str(): Python str() function takes float or int as an argument and returns string type object.
+*/
+
 a=34 
 print(a)
 print(type(a))
