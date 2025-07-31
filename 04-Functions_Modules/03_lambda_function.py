b525467723a5ff359d41ac93d4fa35b09d1eda1b
Shourya1009 <jshourya007@gmail.com>
2025-07-31 23:36:54 +0530

    Updated lambda_functions.py

--- 04-Functions_Modules/03_lambda_function.py
+++ 04-Functions_Modules/03_lambda_function.py
@@ -14,3 +14,19 @@ print(add(3, 5))
 square = lambda x: x*x
 print(square(3))
 # Output: 9
+
+nums = [1, 2, 3, 4, 5, 6]
+evens = list(filter(lambda x: x % 2 == 0, nums))
+print(evens)  # Output: [2, 4, 6]
+
+pairs = [(1, 3), (2, 1), (4, 2)]
+sorted_pairs = sorted(pairs, key=lambda x: x[1])
+print(sorted_pairs)  # Output: [(2, 1), (4, 2), (1, 3)]
+
+check_even_odd = lambda x: "Even" if x % 2 == 0 else "Odd"
+print(check_even_odd(7))  # Output: Odd
+
+
+to_upper = lambda s: s.upper()
+print(to_upper("hello"))  # Output: HELLO
+
