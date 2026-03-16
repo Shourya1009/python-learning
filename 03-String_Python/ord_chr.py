7f8076d130661c43ea11256f0b88f9f9b37b33b5
Shourya1009 <jshourya007@gmail.com>
2026-03-16 23:02:19 +0530

    Update ord_chr.py

--- 03-String_Python/ord_chr.py
+++ 03-String_Python/ord_chr.py
@@ -1,62 +1,36 @@
-# Ord function
-
-# The ord() function returns an integer representing the Unicode character.
+# ------------------------------------
+# ord() Function in Python
+# ------------------------------------
+# The ord() function returns the Unicode value (integer)
+# of a given character.
 
 character = 'P'
 
-# find unicode of P
+# Find Unicode value of 'P'
 unicode_char = ord(character)
-print(unicode_char)
-
-# Output: 80
+print("Unicode of P:", unicode_char)   # Output: 80
 
 
-
-# Ord() Syntax
-
-# The syntax of ord() is:
-# ord(ch)
-
-
-print(ord('5'))    # 53
-print(ord('A'))    # 65
-print(ord('$'))    # 36
-print(ord('S'))
-print(ord('K'))
-print(ord('@'))
-print(ord('*'))
+# Examples of ord()
+print("Unicode of '5':", ord('5'))   # 53
+print("Unicode of 'A':", ord('A'))   # 65
+print("Unicode of '$':", ord('$'))   # 36
+print("Unicode of 'S':", ord('S'))   # 83
+print("Unicode of 'K':", ord('K'))   # 75
+print("Unicode of '@':", ord('@'))   # 64
+print("Unicode of '*':", ord('*'))   # 42
 
 
+# ------------------------------------
+# chr() Function in Python
+# ------------------------------------
+# The chr() function converts a Unicode integer
+# into its corresponding character.
 
-
-
-# chr() function 
-
-# The chr() method converts an integer to its unicode character and returns it.
-
-
-
-# chr() Parameter
-# The chr() method takes in a single parameter:
-# number - an integer number in the range 0 to 1,114,111
-
-print(chr(97))
-# Output: a
-
-print(chr(98))
-#  Output: b
-
-print(chr(65))
-# Output : A
-
-print(chr(89))
-# Ouput - 89
-
-print(chr(58))
-# Output - :
-
-print(chr(165))
-# Output - ¥
-
-print(chr(198))
-# Output - Æ
+print("chr(97):", chr(97))    # a
+print("chr(98):", chr(98))    # b
+print("chr(65):", chr(65))    # A
+print("chr(89):", chr(89))    # Y
+print("chr(58):", chr(58))    # :
+print("chr(165):", chr(165))  # ¥
+print("chr(198):", chr(198))  # Æ
